91df303059ec1e164a3e930282748e28116decd6
Justin Pak <jmp748@nyu.edu>
2016-11-16 18:13:58 -0500

    added forms

--- baconlist/routes/index.js
+++ baconlist/routes/index.js
@@ -3,7 +3,11 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index');
 });
+
+router.get('/share', function(req, res, next) {
+	res.render('share')
+})
 
 module.exports = router;
