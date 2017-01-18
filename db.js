9f19472655c1d2dedf1e52e90621577985c07ce3
Justin Pak <justinpak@Justins-MacBook-Pro.local>
2017-01-18 13:54:00 -0500

    deleted unnecessary directory

--- db.js
+++ db.js
@@ -33,22 +33,4 @@ mongoose.model("User", User);
 mongoose.model("Media", Media);
 mongoose.model("Comments", Comments);
 
-// is the environment variable, NODE_ENV, set to PRODUCTION? 
-if (process.env.NODE_ENV == 'PRODUCTION') {
-	// if we're in PRODUCTION mode, then read the configration from a file
-	// use blocking file io to do this...
-	var fs = require('fs');
-	var path = require('path');
-	var fn = path.join(__dirname, 'config.json');
-	var data = fs.readFileSync(fn);
-
-	// our configuration file will be in json, so parse it and set the
-	// conenction string appropriately!
-	var conf = JSON.parse(data);
-	var dbconf = conf.dbconf;
-} else {
-	 // if we're not in PRODUCTION mode, then use
-	 dbconf = 'mongodb://localhost/jmp748';
-}
-
-mongoose.connect(dbconf);
+mongoose.connect('mongodb://localhost/jmp748');
